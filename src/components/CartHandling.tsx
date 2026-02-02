25d4ad84cc8983d1bb518bf574aaa0082c719e03
eryk <e.szczepaniec@lightpixel.io>
2026-02-02 14:12:54 +0100

    fix(ui): add gap between elements

--- src/components/CartHandling.tsx
+++ src/components/CartHandling.tsx
@@ -22,8 +22,9 @@ const CartHandling: React.FC<CartStateProps> = ({
   };
 
   return (
-    <div className="md:flex flex-wrap items-center">
-      <div className="p-4 flex flex-wrap justify-between items-center bg-[#f7f8fdff] rounded-lg md:w-1/3">
+    <div className="flex flex-col md:flex-row md:items-center md:gap-4">
+      {/* handle item quantity */}
+      <div className="p-4 flex items-center justify-between bg-[#f7f8fdff] rounded-lg w-full md:w-1/3">
         <img
           src={minusIcon}
           alt="Decrease Item Quantity"
@@ -38,9 +39,11 @@ const CartHandling: React.FC<CartStateProps> = ({
           className="cursor-pointer"
         />
       </div>
+
+      {/* add item to cart */}
       <button
         type="button"
-        className="py-4 my-4 w-full h-auto rounded-lg bg-orange-500 hover:bg-orange-300 font-semibold shadow-lg shadow-orange-500/50 flex items-center justify-center gap-2 md:w-2/3"
+        className="py-4 my-4 w-full md:flex-1 h-auto rounded-lg bg-orange-500 hover:bg-orange-300 font-semibold shadow-lg shadow-orange-500/50 flex items-center justify-center gap-2"
         onClick={() => product && addToCart(product, itemQuantity)}
         disabled={!product || itemQuantity <= 0}
       >
